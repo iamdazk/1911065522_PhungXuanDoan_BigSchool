bd5899be9b4d6d1d41a29276f3d612b41cef22c1
Phùng Xuân Đoan <kanpitest@gmail.com>
2022-03-21 08:39:08 +0700

    update 2

---
+++ 1911065522_PhungXuanDoan_BigSchool/1911065522_PhungXuanDoan_BigSchool/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace _1911065522_PhungXuanDoan_BigSchool.Models
+{
+    public class Course : Category
+    {
+        public int Id { get; set; }
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        public string LecturerId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public Category Category { get; set; }
+        public DateTime DateTime { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+    }
+}
