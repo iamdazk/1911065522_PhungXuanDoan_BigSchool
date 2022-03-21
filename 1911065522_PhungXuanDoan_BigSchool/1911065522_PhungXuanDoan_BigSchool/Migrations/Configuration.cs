8ce616c38a2267681d6445b7d6a256a53dbf7dac
Phùng Xuân Đoan <kanpitest@gmail.com>
2022-03-21 11:08:27 +0700

    commit trang 26 luc 11h08

---
+++ 1911065522_PhungXuanDoan_BigSchool/1911065522_PhungXuanDoan_BigSchool/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace _1911065522_PhungXuanDoan_BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<_1911065522_PhungXuanDoan_BigSchool.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "_1911065522_PhungXuanDoan_BigSchool.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(_1911065522_PhungXuanDoan_BigSchool.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
