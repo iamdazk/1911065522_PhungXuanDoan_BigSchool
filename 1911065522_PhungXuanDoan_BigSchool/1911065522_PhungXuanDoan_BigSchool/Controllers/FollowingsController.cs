112982203f829ef86f75f6d7287995e8b8f23e77
Phùng Xuân Đoan <kanpitest@gmail.com>
2022-03-28 00:33:03 +0700

    folow

---
+++ 1911065522_PhungXuanDoan_BigSchool/1911065522_PhungXuanDoan_BigSchool/Controllers/FollowingsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _1911065522_PhungXuanDoan_BigSchool.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+    }
+}
--- 1911065522_PhungXuanDoan_BigSchool/1911065522_PhungXuanDoan_BigSchool/Models/ApplicationDbContext.cs
+++ 1911065522_PhungXuanDoan_BigSchool/1911065522_PhungXuanDoan_BigSchool/Models/ApplicationDbContext.cs
@@ -5,17 +5,18 @@ namespace _1911065522_PhungXuanDoan_BigSchool.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-            public DbSet<Course> Courses { get; set; }
-            public DbSet<Category> Categories { get; set; }
-            public DbSet<Attendance> Attendances { get; set; }
-            public ApplicationDbContext()
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
+        public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
         public static ApplicationDbContext Create()
         {
-            
+
             return new ApplicationDbContext();
         }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
@@ -24,14 +25,14 @@ namespace _1911065522_PhungXuanDoan_BigSchool.Models
                 .HasRequired(a => a.Course)
                 .WithMany()
                 .WillCascadeOnDelete(false);
-            /*modelBuilder.Entity<ApplicationUser>()
+            modelBuilder.Entity<ApplicationUser>()
                 .HasMany(u => u.Followers)
                 .WithRequired(f => f.Followee)
                 .WillCascadeOnDelete(false);
             modelBuilder.Entity<ApplicationUser>()
                 .HasMany(u => u.Followees)
                 .WithRequired(f => f.Follower)
-                .WillCascadeOnDelete(false);*/
+                .WillCascadeOnDelete(false);
             base.OnModelCreating(modelBuilder);
         }
     }
